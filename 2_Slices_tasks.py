9171242fc80301f92be715ec9d7b2481519f907f
maxelame <maxelame31@gmail.com>
2022-05-10 09:22:31 +0300

    slices tasks finally

--- 2_Slices_tasks.py
+++ 2_Slices_tasks.py
@@ -55,22 +55,3 @@ print(number[:5] + "?" *5 + number[-2:])
 import sys
 number = sys.argv[1]
 print("+7", "("+number[1:4]+")", number[4:7]+"-"+number[7:9]+"-"+number[9:])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
