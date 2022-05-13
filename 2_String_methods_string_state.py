322b18c7d59a69fe7094500c5fed55c585b0898d
maxelame <maxelame31@gmail.com>
2022-05-13 08:57:31 +0300

    add tasks

--- 2_String_methods_string_state.py
+++ 2_String_methods_string_state.py
@@ -58,7 +58,57 @@ file = sys.argv[1]
 file = file.lower()
 print(file.endswith(".png"))
 # =========================================================
+Это изображение?
+Введение
 
+Мы уже познакомились с логическим типом, а также с оператором not, который меняет логическое значение на обратное. Давайте теперь изучим логическую операцию or.
+
+Логическая операция or сравнивает несколько значений и возвращает True, если хотя бы одно из них будет логически верным:
+# Создаем четыре логические переменные.
+is_male = True
+is_female = False
+is_old = True
+is_young = False
+
+# Сравниваем: True или False.
+# Получаем True, так как одна из переменных True.
+print(is_male or is_female)
+True
+
+# Сравниваем: False или True (поменяли местами).
+# Всё равно получаем True, так как одна из переменных True.
+print(is_female or is_male)
+True
+
+# Сравниваем: False или False.
+# Получаем False, так как нет True.
+print(is_young or is_female)
+False
+
+# Сравниваем все переменные.
+print(is_young or is_female or is_old or is_young)
+True
+
+Задание
+
+Напишите программу, которая первым аргументом командной строки принимает название файла, а после выводит True если это изображение, и False в противном случае.
+Определять изображение это или нет нужно по расширению файла: png, jpeg, jpg или gif.
+Учитывайте, что имя может быть набрано в разных регистрах.
+Пример использования в командной строке Windows:
+> python program.py photo.png
+> True
+> python program.py photo.jpg
+> True
+> python program.py ROOM.PNG
+> True
+> python program.py video.mp4
+> False
+
+
+import sys
+file_name = sys.argv[1]
+file_name = file_name.lower()
+print(file_name.endswith(".png") or file_name.endswith(".jpg") or file_name.endswith(".jpeg") or file_name.endswith(".gif"))
 
 
 
