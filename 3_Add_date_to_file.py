9f92e737408b643a09a51aa0d9f2b5e72854fd35
maxelame <maxelame31@gmail.com>
2022-05-22 23:43:49 +0300

    add task

--- 3_Add_date_to_file.py
+++ 3_Add_date_to_file.py
@@ -42,3 +42,4 @@ products_file = open ("products.txt", "a", encoding = "cp1251")
 new_products_file = open ("new_products.txt", encoding = "koi8-r")
 products = new_products_file.read()
 products_file.write(products +"\n")
+
