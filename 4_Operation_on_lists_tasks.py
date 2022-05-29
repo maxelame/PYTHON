e036e29cbd9063fd430515891e6ee360aa5ad664
maxelame <maxelame31@gmail.com>
2022-05-29 07:39:53 +0300

    add task

--- 4_Operation_on_lists_tasks.py
+++ 4_Operation_on_lists_tasks.py
@@ -13,7 +13,7 @@ print(j)
 
 
 import sys
-
+mention, repeat = sys.argv[1], int(sys.argv[2])
 '''Расширяем города
 
 Аргументы командной строки, которые передаются в программу, хранятся в списке sys.argv.
