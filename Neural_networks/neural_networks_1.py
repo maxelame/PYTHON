ad03934d982a603ae4de4f18e473dd8ba911a030
maxelame <maxelame31@gmail.com>
2022-06-13 07:19:28 +0300

    add

--- Neural_networks/neural_networks_1.py
+++ Neural_networks/neural_networks_1.py
@@ -56,4 +56,14 @@ x = np.array([2, 3])
 print(network.feedforward(x)) # 0.7216325609518421
 
 
-
+'''
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**', end='')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='\n')
+print('m', 'n', 'o', sep='/', end='!')
+print('p', 'q', 'r', sep='1', end='%')
+print('s', 't', 'u', sep='&', end='\n')
+print('v', 'w', 'x', sep='%')
+print('y', 'z', sep='/', end='!')
+'''
