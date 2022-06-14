7d1a16f4ed43837e858ca72b822689c9f7033be2
maxelame <maxelame31@gmail.com>
2022-06-14 08:32:46 +0300

    add

--- Shultais_Education/4_Functions_for_working_with_lists.py
+++ Shultais_Education/4_Functions_for_working_with_lists.py
@@ -21,7 +21,14 @@ print(len(arg))
 > python program.py 6
 > 278.46
  '''
+import sys
 
+tax = float(sys.argv[1])
+
+revenue = [178, 351, 145, 764, 514, 456,
+           411, 145, 275, 245, 441, 716]
+
+print(float(sum(revenue) * tax / 100))
 
 
 
