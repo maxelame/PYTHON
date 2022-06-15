7e0dad6d9e15ccc7cb6db70ca175327c43285419
maxelame <maxelame31@gmail.com>
2022-06-15 08:38:31 +0300

    add

--- Shultais_Education/4_List_methods.py
+++ Shultais_Education/4_List_methods.py
@@ -133,4 +133,3 @@ school_marks = [3, 4, 4, 5, 3, 3, 5, 5, 5, 4, 3, 2, 4, 5, 2, 4, 5]
 count_mark = school_marks.count(mark)
 #total_mark = len(school_marks)
 print("{:.1f}%" .format(count_mark*100/len(school_marks)))
-
