3f9ad4676c92f9e3bebd0b946a05fe46744e021d
maxelame <maxelame31@gmail.com>
2022-06-15 08:31:59 +0300

    add

--- Shultais_Education/4_List_methods.py
+++ Shultais_Education/4_List_methods.py
@@ -5,3 +5,9 @@
 > python program.py Nissan
 > ['BMW', 'Toyota', 'Mercedes', 'Nissan']
  '''
+
+import sys
+new_mark = sys.argv[1]
+marks = ["BMW", "Toyota", "Mercedes"]
+marks.append(new_mark)
+print(marks)
