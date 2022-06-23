6bc0ce78606efc2351942853572407d2d853ce2f
maxelame <maxelame31@gmail.com>
2022-06-23 08:16:37 +0300

    add

--- Shultais_Education/5_dictionaries.py
+++ Shultais_Education/5_dictionaries.py
@@ -68,5 +68,23 @@ print(sum(quarters[key]))
 > четыре
  '''
 
+import sys
+# Принимаем цифру и язык
+number, language = int(sys.argv[1]), sys.argv[2].lower().strip()
+digits = {
+    1: {"ru": "один", "en": "one"},
+    2: {"ru": "два", "en": "two"},
+    3: {"ru": "три", "en": "three"},
+    4: {"ru": "четыре", "en": "four"},
+    5: {"ru": "пять", "en": "five"},
+    6: {"ru": "шесть", "en": "six"},
+    7: {"ru": "семь", "en": "seven"},
+    8: {"ru": "восемь", "en": "eight"},
+    9: {"ru": "девять", "en": "nine"},
+    0: {"ru": "ноль", "en": "zero"}
+}
+print(digits[number][language])
+
+
 
 
