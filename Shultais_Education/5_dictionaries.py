6a39fc7b1bb93860858b2412f805176e086d6086
maxelame <maxelame31@gmail.com>
2022-06-24 07:55:25 +0300

    modified

--- Shultais_Education/5_dictionaries.py
+++ Shultais_Education/5_dictionaries.py
@@ -219,5 +219,30 @@ print(user)
 > {'model': 'UE32M5550AU', 'brand': 'Samsung', 'price': 23990, 'count': 3}'''
 
 
+import sys
+
+# Получаем данные.
+new_model = sys.argv[1]
+new_price = int(sys.argv[2])
+new_count = int(sys.argv[3])
+
+# Исходный словарь.
+product = {
+    "model": "UE43NU7097U",
+    "brand": "Samsung",
+    "price": 27990,
+    "count": 7
+}
+
+# Обновляем данные.
+#product["count"] = new_count
+product.update(model = new_model, price = new_price, count = new_count)
+
+# Выводим результат.
+print(product)
+
+
+
+
 
 
