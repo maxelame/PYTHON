aa9cd976d8647897801de20a278ecfe9d1df651c
maxelame <maxelame31@gmail.com>
2022-06-26 22:42:16 +0300

    add

--- Shultais_Education/5_dictionaries.py
+++ Shultais_Education/5_dictionaries.py
@@ -303,6 +303,16 @@ print(product)
 > неизвестно
  '''
 
+import sys
+key = sys.argv[1]
+car = {
+    "mark": "Nissan",
+    "model": "Qashqai",
+    "year": 2018,
+    "price": 1_200_000,
+    'volume': 2.0
+}
+print(car.get(key, "неизвестно") or "неизвестно")
 
 
 
