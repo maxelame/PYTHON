f473c9369c6a4be40ff05e7a9831e470c8febdf8
maxelame <maxelame31@gmail.com>
2022-06-29 07:52:45 +0300

    add

--- Shultais_Education/8_.py
+++ Shultais_Education/8_.py
@@ -244,3 +244,31 @@ else:
 Если длина более 8 символов — «Пароль подходит».
  
 '''
+
+import sys
+
+password = sys.argv[1]
+
+# Используем функцию len()
+if len(password) < 6:
+    print("Пароль слишком короткий")
+# Используем метод __len__()
+elif 6 <= password.__len__() <= 8:
+    print("Хорошо, но можно и лучше")
+else:
+    print("Пароль подходит")
+
+# Функция len() и метод __len__() равнозначны
+
+
+"""
+import sys
+password = sys.argv[1]
+len_password = len(password)
+if len_password > 8 :
+    print("Пароль подходит")
+elif len_password > 5 :
+    print("Хорошо, но можно и лучше")
+else:
+    print("Пароль слишком короткий")
+"""
