f519ab92505e4227cc705f80086443e603287c13
maxelame <maxelame31@gmail.com>
2022-06-29 07:51:47 +0300

    add

--- Shultais_Education/8_.py
+++ Shultais_Education/8_.py
@@ -193,3 +193,46 @@ print(", ".join(products))
 > Доступ открыт
  '''
 
+"""
+import sys
+user, password = sys.argv[1], sys.argv[2]
+users = {
+    "user1": "password1",
+    "user2": "abcde",
+    "user3": "123456",
+    "user4": "lovelove",
+    "user5": "kdmUdmk84M",
+}
+if user not in users:
+    print ("Пользователь не найден")
+elif user in users:
+    if users.get(user) == password:
+        print ("Доступ открыт")
+    else:
+        print ("Доступ закрыт")
+"""
+import sys
+
+users = {
+    "user1": "password1",
+    "user2": "abcde",
+    "user3": "123456",
+    "user4": "lovelove",
+    "user5": "kdmUdmk84M",
+}
+
+# Получаем имя пользователя.
+username = sys.argv[1]
+password = sys.argv[2]
+
+# Получаем пароль пользователя.
+user_password = users.get(username)
+
+if user_password is None:
+    print("Пользователь не найден")
+elif user_password == password:
+    print("Доступ открыт")
+else:
+    print("Доступ закрыт")
+
+
