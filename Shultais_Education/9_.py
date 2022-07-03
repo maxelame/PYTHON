2db10f909ede8f6d4cf33a9556587d3acccf6798
maxelame <maxelame31@gmail.com>
2022-07-03 07:48:46 +0300

    add

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -210,6 +210,13 @@ else:
 
 Заполните список numbers нечетными числами от 11 до 27 включительно. Используйте цикл while.'''
 
+numbers = []
+j = 11
+while j <=27:
+    if j % 2 == 1:
+        numbers.append(j)
+    j += 1
+print(numbers)
 
 
 
