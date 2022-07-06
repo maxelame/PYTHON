9d03f5171d5bcb9b3a13ac6e5776fe23aafa7617
maxelame <maxelame31@gmail.com>
2022-07-06 07:19:53 +0300

    add

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -354,3 +354,30 @@ print(max_num)
 > python program.py 200
 > Гречка, Хлеб, Молоко
  '''
+
+import sys
+
+money = int(sys.argv[1])
+products = [
+    {"name": "Гречка", "price": 69},
+    {"name": "Хлеб", "price": 34},
+    {"name": "Молоко", "price": 57},
+    {"name": "Яйца", "price": 78},
+    {"name": "Рис", "price": 88},
+    {"name": "Макароны", "price": 49},
+    {"name": "Сахар", "price": 22},
+    {"name": "Яблоки", "price": 79},
+    {"name": "Картофель", "price": 18},
+    {"name": "Свинина", "price": 120},
+    {"name": "Масло", "price": 66},
+    {"name": "Помидоры", "price": 64}
+]
+busket = []
+for i in products:
+    if money >= int(i["price"]):
+        money -= int(i["price"])
+        busket.append(i["name"])
+    else:
+        break
+
+print(", ".join(busket))
