b6b812b2465f4bf2b2057ec602903998ad697cca
maxelame <maxelame31@gmail.com>
2022-07-08 23:46:37 +0300

    add

---
+++ Python_Generation/Block_2_1.py
@@ -0,0 +1,23 @@
+""">>> 2.1 Блок подсчета процентного перевеса четных тиков над нечетными и наоборот"""
+
+# создадим простой генератор тиков
+
+import time
+import random
+from collections import Counter
+ticks_list = []
+while True:
+    time.sleep(1)
+    r_n = random.randint(0, 1)  # r_n - random number
+    ticks_list.append(r_n)
+    last_25 = ticks_list[-25:]
+    print(last_25)
+    c = Counter(last_25)
+    if c[0] != 0 and c[1] != 0:
+        percent_over = int(c[0]*100/(c[1] + c[0]))
+        percent_odd = 100 - percent_over
+        print(f"{c}-----{c[0]}----{c[1]}----{percent_over}%----{percent_odd}%")
+
+
+
+
