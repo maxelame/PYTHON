69548ea8ba09393d76def87a3d0c740586ed7df6
maxelame <maxelame31@gmail.com>
2022-07-19 08:21:48 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1234,3 +1234,12 @@ print(color[t.index(x)] if x in t else 'ошибка цвета')
 
 Формат выходных данных
 Программа должна вывести на экран границы отрезка, являющегося пересечением, либо общую точку, либо текст «пустое множество».'''
+
+
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+if min(b1, b2) < max(a1, a2):
+    print('пустое множество')
+elif min(b1, b2) == max(a1, a2):
+    print(min(b1, b2))
+else:
+    print(max(a1, a2), min(b1, b2))
