b7e588802324e07b5ffe5dbdf83520af1c727459
maxelame <maxelame31@gmail.com>
2022-07-20 09:04:48 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1305,3 +1305,9 @@ else:
 
 Формат выходных данных
 Программа должна вывести текст в соответствии с условием задачи.'''
+
+num = int(input())
+if num % 2 == 1 or (num % 2 == 0 and 6 <= num <= 20):
+    print("YES")
+elif (num % 2 == 0 and 2 <= num <= 5) or (num % 2 == 0 and num >20):
+    print("NO")
