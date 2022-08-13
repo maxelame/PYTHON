d0c7ee9104f1fb6c97344881110e3189b785527e
maxelame <maxelame31@gmail.com>
2022-08-13 09:57:46 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1515,3 +1515,11 @@ print((a+b)/2)
 print(sqrt(a*b))
 print(2*a*b/(a+b))
 print(sqrt((a ** 2 + b ** 2)/2))
+
+
+from math import *
+a, b = float(input()), float(input())
+print((a+b)/2)
+print(sqrt(a*b))
+print(2*a*b/(a+b))
+print(sqrt((a ** 2 + b ** 2)/2))
