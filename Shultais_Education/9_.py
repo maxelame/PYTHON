557f8566d4ae00682c0327ea3a4060a326a121e2
maxelame <maxelame31@gmail.com>
2022-08-13 09:55:48 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1507,3 +1507,11 @@ from math import *
 R = float(input())
 print(pi*pow(R, 2))
 print(2*pi*R)
+
+
+from math import *
+a, b = float(input()), float(input())
+print((a+b)/2)
+print(sqrt(a*b))
+print(2*a*b/(a+b))
+print(sqrt((a ** 2 + b ** 2)/2))
