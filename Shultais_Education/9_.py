466266935dff83c8178ce9c5d559ec048dca4568
maxelame <maxelame31@gmail.com>
2022-08-14 12:48:43 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1523,3 +1523,9 @@ print((a+b)/2)
 print(sqrt(a*b))
 print(2*a*b/(a+b))
 print(sqrt((a ** 2 + b ** 2)/2))
+
+
+from math import *
+x = float(input())
+r = radians(x)
+print(sin(r) + cos(r) + tan(r) ** 2)
