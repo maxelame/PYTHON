97e93473ecfa7af95e1815b2218193ae034dbfdc
maxelame <maxelame31@gmail.com>
2022-08-14 13:25:36 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1534,3 +1534,17 @@ print(sin(r) + cos(r) + tan(r) ** 2)
 from math import floor, ceil
 x = float(input())
 print(floor(x) + ceil(x))
+
+
+from math import *
+a, b, c = float(input()), float(input()), float(input())
+D = b ** 2 - 4*a*c
+if D < 0:
+    print("Нет корней")
+elif D == 0:
+    print(-b/(2*a))
+else:
+    x1 = (-b-sqrt(D))/(2*a)
+    x2 = (-b+sqrt(D))/(2*a)
+    print(min(x1, x2))
+    print(max(x1, x2))
