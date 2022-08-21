f2c865bdf7d056da91272e7ce568901cafe41232
maxelame <maxelame31@gmail.com>
2022-08-21 09:37:17 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1645,3 +1645,8 @@ m, n = int(input()), int(input())
 for i in range(m, n + 1):
     if i % 17 == 0 or i % 10 == 9 or (i % 3 == 0 and i % 5 == 0):
         print(i)
+
+
+n = int(input())
+for i in range(1, 11):
+    print(f"{n} x {i} = {n * i}")
