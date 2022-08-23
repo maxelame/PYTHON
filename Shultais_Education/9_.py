2ca9117d4e3ac6ff2ef813b47ce0004249335eb8
maxelame <maxelame31@gmail.com>
2022-08-23 07:05:07 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1685,3 +1685,12 @@ summ = 0
 for i in range(n):
     summ += int(input())
 print(summ)
+
+
+
+from math import *
+n = int(input())
+summ =  - log(n)
+for i in range(1, n+1):
+    summ += 1/i
+print(summ)
