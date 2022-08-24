6933b42092144ba49695fac12e64c0b693e1d1b2
maxelame <maxelame31@gmail.com>
2022-08-24 07:03:00 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1694,3 +1694,11 @@ summ =  - log(n)
 for i in range(1, n+1):
     summ += 1/i
 print(summ)
+
+
+n = int(input())
+summ = 0
+for i in range(1, n +1):
+    if i ** 2 % 10 == 2 or i ** 2 % 10 == 5 or i ** 2 % 10 == 8:
+        summ += i
+print(summ)
