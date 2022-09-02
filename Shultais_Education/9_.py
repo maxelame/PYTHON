5cdab22b9b5ec4f31a55ad72212865cefe27ccaa
maxelame <maxelame31@gmail.com>
2022-09-02 08:26:30 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1865,3 +1865,13 @@ while num != 0:
     product = product * last_digit
     num = num // 10
 print(product)
+
+
+num = 123456789
+total = 0
+while num != 0:
+    last_digit = num % 10
+    if last_digit > 4:
+        total += 1
+    num = num // 10
+print(total)
