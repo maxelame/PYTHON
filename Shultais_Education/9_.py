168a202fab407af112c0dc3001b26e150dea26e9
maxelame <maxelame31@gmail.com>
2022-09-02 08:16:00 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -1857,3 +1857,11 @@ while cost > 0:
 print(count)
 
 
+
+num = 12345
+product = 1
+while num != 0:
+    last_digit = num % 10
+    product = product * last_digit
+    num = num // 10
+print(product)
