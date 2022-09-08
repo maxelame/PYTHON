39f6f2a8ba8a5509cb7b970058a3f680a2b02e20
maxelame <maxelame31@gmail.com>
2022-09-08 09:03:34 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2062,3 +2062,18 @@ while n > 0:
     if n == 2:
         continue
     print(n, end='*')
+
+
+
+
+ Что покажет приведенный ниже фрагмент кода?
+
+mult = 1
+for i in range(1, 11):
+   if i % 2 == 0:
+      continue
+   if i % 9 == 0:
+      break
+   mult *= i
+print(mult)
+
