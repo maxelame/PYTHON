c3681d3adacaeed594d52a90f31367a4c4d1d9c5
maxelame <maxelame31@gmail.com>
2022-09-10 11:39:38 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2099,3 +2099,14 @@ while n < 10:
     print(n)
 else:
     print('Цикл завершен.')
+
+
+
+n = 0
+while n < 10:
+    n += 2
+    if n == 8:
+        break
+    print(n)
+else:
+    print('Цикл завершен.')
