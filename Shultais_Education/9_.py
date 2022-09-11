4667150c8e6416119a0074fc056cf7640e7bfb4a
maxelame <maxelame31@gmail.com>
2022-09-11 13:23:45 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2221,3 +2221,15 @@ else:
 
 
 
+count = 0
+p = 1
+for i in range(0, 10):
+    x = int(input())
+    if x > -1:
+        p *= x
+        count += 1
+if count > 0:
+    print(count)
+    print(p)
+else:
+    print('NO')
