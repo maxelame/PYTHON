e731d83cb610a6fc17e543e21689cf4f68992512
maxelame <maxelame31@gmail.com>
2022-09-12 17:34:43 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2248,3 +2248,18 @@ if s < 0:
     print(mx)
 else:
     print("NO")
+
+
+mx = float("-inf")
+s = 0
+for i in range(10):
+    x = int(input())
+    if x < 0:
+        s += x
+        if x > mx:
+            mx = x
+if s < 0:
+    print(s)
+    print(mx)
+else:
+    print("NO")
