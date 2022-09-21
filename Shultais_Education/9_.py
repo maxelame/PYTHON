0b18733e762f17694e66953a25b54c90067b1af4
maxelame <maxelame31@gmail.com>
2022-09-21 08:41:36 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2390,3 +2390,13 @@ for i in range(1,a+1):
 n = int(input())
 for i in range(1, n + 1):
     print(str(i) * i)
+
+
+
+total = 0
+for x in range(1, 65):
+    for y in range(1, 60):
+        if 12 * x + 13 * y == 777:
+            total += 1
+            print('x =', x, 'y =', y)
+print('Общее количество натуральных решений =', total)
