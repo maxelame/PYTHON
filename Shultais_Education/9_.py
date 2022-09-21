7f7c1b2673b32ac141160fd150268339bf7d677d
maxelame <maxelame31@gmail.com>
2022-09-21 08:43:21 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2400,3 +2400,13 @@ for x in range(1, 65):
             total += 1
             print('x =', x, 'y =', y)
 print('Общее количество натуральных решений =', total)
+
+
+total = 0
+for x in range(1, 45):
+    for y in range(1, 45):
+        for z in range(1, 45):
+            if x ** 2 + y ** 2 + z ** 2 == 2020:
+                total += 1
+                print('x =', x, 'y =', y, 'z =', z)
+print('Общее количество натуральных решений =', total)
