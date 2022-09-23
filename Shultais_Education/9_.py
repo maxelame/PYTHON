1b2327d26d44d6bdb20f03de2ef5e495fac464de
maxelame <maxelame31@gmail.com>
2022-09-23 22:59:24 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2485,3 +2485,34 @@ for i in range(1, n+1):
         print(l, end = " ")
     k = l + 1
     print()
+
+
+
+for row in range(1, 30):
+    for i in range(30 - row):
+        print(' ', end=' ')
+    for i in range(1, row):
+        if i < 10:
+            print(i, end=' ')
+        else:
+            print(i, end='')
+    for i in range(row, 0, -1):
+        if i < 10:
+            print(i, end=' ')
+        else:
+            print(i, end='')
+    print()
+for j in range(30):
+    for i in range(j + 2):
+        print(' ', end=' ')
+    for i in range(1, 29 - j):
+        if i < 10:
+            print(i, end=' ')
+        else:
+            print(i, end='')
+    for i in range(27 - j, 0, -1):
+        if i < 10:
+            print(i, end=' ')
+        else:
+            print(i, end='')
+    print()
