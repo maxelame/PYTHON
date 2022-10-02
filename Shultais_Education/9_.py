ff8ce36884aee46cb83395083319908104156e7c
maxelame <maxelame31@gmail.com>
2022-10-02 11:09:00 +0300

    new task

--- Shultais_Education/9_.py
+++ Shultais_Education/9_.py
@@ -2661,3 +2661,7 @@ for a in range(1, 34):
             for d in range(1, 34):
                 if a ** 3 + b ** 3 == c ** 3 + d ** 3 and a != b and c != d and a != c and b != d and a != d and b != c:
                     print(a, b, c, d, a ** 3 + b ** 3)
+
+
+for i in input()[::-1]: # с помощью [::-1] инвертируем строку
+    print(i)
