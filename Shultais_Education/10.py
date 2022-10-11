3f88e5a7e6c563b970317fa3e29000aa11d8833b
maxelame <maxelame31@gmail.com>
2022-10-11 08:53:57 +0300

    new task

--- Shultais_Education/10.py
+++ Shultais_Education/10.py
@@ -3,3 +3,10 @@ for i in s:
     if i.islower(): # метод строк islower() проверяет в нижнем ли регистре буква, isupper() - в верхнем
         count += 1
 print(count)
+
+x = input()
+counter_lower = 0
+for char in x:
+    if char != char.upper():
+        counter_lower += 1
+print(counter_lower)
