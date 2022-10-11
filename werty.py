4603af8deab989f5f5261669ea3f11def87aabda
maxelame <maxelame31@gmail.com>
2022-10-11 08:54:52 +0300

    new task

--- werty.py
+++ werty.py
@@ -1,1 +1,6 @@
-print(9 // 10)
+for a in range(1, 34):
+    for b in range(a, 34):
+        for c in range(b, 34):
+            for d in range(c, 34):
+                if a ** 3 + b ** 3 == c ** 3 + d ** 3 and a != b and c != d and a != c and b != d and a != d and b != c:
+                    print(a, b, c, d, a ** 3 + b ** 3)
