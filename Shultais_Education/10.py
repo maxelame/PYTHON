3ec949f693f86cb59ba569fbe52f7f9929680350
maxelame <maxelame31@gmail.com>
2022-10-12 07:30:50 +0300

    new task

--- Shultais_Education/10.py
+++ Shultais_Education/10.py
@@ -24,3 +24,14 @@ print(len(input().split()))
 
 s = input()
 print(s.count(" ") + 1)
+
+
+g_code, count_A, count_G, count_C, count_T = input(), 0, 0, 0, 0
+count_A = g_code.count("А") + g_code.count("а")
+count_G = g_code.count("Г") + g_code.count("г")
+count_C = g_code.count("Ц") + g_code.count("ц")
+count_T = g_code.count("Т") + g_code.count("т")
+print(f"Аденин: {count_A}")
+print(f"Гуанин: {count_G}")
+print(f"Цитозин: {count_C}")
+print(f"Тимин: {count_T}")
