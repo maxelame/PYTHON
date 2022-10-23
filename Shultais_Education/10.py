6232b1d4fb71f2db9adb2731aea890202d205b92
maxelame <maxelame31@gmail.com>
2022-10-23 07:58:16 +0300

    new task

--- Shultais_Education/10.py
+++ Shultais_Education/10.py
@@ -260,3 +260,8 @@ print(list(abc))
 
 
 print(list(map(chr, range(97, 97 + int(input())))))
+
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+
+print(primes[16])
