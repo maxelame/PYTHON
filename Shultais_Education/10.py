a42fcd02a315754690a2961f98ebf1be8de28909
maxelame <maxelame31@gmail.com>
2022-10-24 07:14:37 +0300

    new task

--- Shultais_Education/10.py
+++ Shultais_Education/10.py
@@ -271,3 +271,10 @@ evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 average = sum(evens) / len(evens)
 
 print(average)
+
+
+
+rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+rainbow[3] = "Зеленый"
+rainbow[-1] = "Фиолетовый"
+print(rainbow)
