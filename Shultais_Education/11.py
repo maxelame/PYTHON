15b767b25196b0c5ce97e5244b23955a820c98fb
maxelame <maxelame31@gmail.com>
2022-10-25 08:29:59 +0300

    new task

--- Shultais_Education/11.py
+++ Shultais_Education/11.py
@@ -8,3 +8,10 @@ for i in range(0, len(languages) // 2):
     languages[i], languages[-(i + 1)] = languages[-(i + 1)], languages[i]
 
 print(languages)
+
+
+numbers1 = [1, 2, 3]
+numbers2 = [6]
+numbers3 = [7, 8, 9, 10, 11, 12, 13]
+
+print(numbers1 * 2 + numbers2 * 9 + numbers3)
