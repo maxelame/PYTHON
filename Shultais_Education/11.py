2627de565cd2371616278df042270bebb780f020
maxelame <maxelame31@gmail.com>
2022-10-25 08:27:41 +0300

    new task

--- Shultais_Education/11.py
+++ Shultais_Education/11.py
@@ -1,2 +1,10 @@
 languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
 print(languages[::-1])
+
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+
+for i in range(0, len(languages) // 2):
+    languages[i], languages[-(i + 1)] = languages[-(i + 1)], languages[i]
+
+print(languages)
