be20106db5ca5c5b863c0eb763f98a23d86e53d4
maxelame <maxelame31@gmail.com>
2022-10-31 07:01:32 +0300

    new task

--- Shultais_Education/11.py
+++ Shultais_Education/11.py
@@ -175,3 +175,9 @@ s = []
 for _ in range(n):
     s.extend(list(input()))
 print(s)
+
+
+
+
+numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+print(sum([i ** 2 for i in numbers]))
