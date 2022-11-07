feba473941c6e468cfc6f6a173b413b3ec6bb087
maxelame <maxelame31@gmail.com>
2022-11-07 10:43:54 +0300

    new task

--- Shultais_Education/12.py
+++ Shultais_Education/12.py
@@ -1,3 +1,7 @@
 print(*[i[0]+"." for i in input().split()], sep="")
 
 print("\n".join(input().split('\\'))) # не забываем, что одна \ это символ экранирования
+
+
+
+print(*input().split(chr(92)), sep='\n')
