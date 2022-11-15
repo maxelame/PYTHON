e3467a0bedaf20624010a404965d4d7ff560254e
maxelame <maxelame31@gmail.com>
2022-11-15 06:59:30 +0300

    new task

--- Shultais_Education/12.py
+++ Shultais_Education/12.py
@@ -271,3 +271,11 @@ s_list.sort()
 print(*s_list, sep = " ")
 s_list.sort(reverse = True)
 print(*s_list, sep = " ")
+
+
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+new_keywords = [s[1:] for s in keywords]
+
+print(new_keywords)
