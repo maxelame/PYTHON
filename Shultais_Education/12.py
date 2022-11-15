41625d2560f501cdde21d441ff2ce27181c94956
maxelame <maxelame31@gmail.com>
2022-11-15 18:05:13 +0300

    new task

--- Shultais_Education/12.py
+++ Shultais_Education/12.py
@@ -277,3 +277,10 @@ print(*s_list, sep = " ")
 keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
 new_keywords = [s[1:] for s in keywords]
 print(new_keywords)
+
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+
+lengths = [len(s) for s in keywords]
+
+print(lengths)
