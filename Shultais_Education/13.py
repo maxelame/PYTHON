fb18db83cf10771efab1dfea9be6d641138b6c23
maxelame <maxelame31@gmail.com>
2022-11-25 07:24:49 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -81,3 +81,17 @@ name, surname, patronymic = input(), input(), input()
 
 # вызываем функцию
 print_fio(name, surname, patronymic)
+
+
+# объявление функции
+def print_digit_sum(num):
+    sum = 0
+    for i in list(str(num)):
+        sum += int(i)
+    print(sum)
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print_digit_sum(n)
