3cbfb67665303d5ad4a65f000aec0f0edcc817ed
maxelame <maxelame31@gmail.com>
2022-11-27 09:14:10 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -109,3 +109,28 @@ def print_paris():
     s = 'I love Paris'
 
 print_paris()
+
+
+
+
+
+
+number = 101
+
+def is_prime(num):
+    flag = True
+    for i in range(2, num):
+        if num % i == 0:
+            flag = False
+            break
+    if num != 1 and flag == True:
+        print('Число', num, 'простое.')
+    else:
+        print('Число', num, 'составное.')
+
+
+x = 17
+y = int(input())
+is_prime(x)
+is_prime(y)
+is_prime(number)
