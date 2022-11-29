f09d1790f638945788b01fcf8d3746aaa1f57d25
maxelame <maxelame31@gmail.com>
2022-11-29 09:23:28 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -163,9 +163,7 @@ def compute_average(numbers):
 # объявление функции
 def convert_to_miles(km):
     return km * 0.6214
-
 # считываем данные
 num = int(input())
-
 # вызываем функцию
 print(convert_to_miles(num))
