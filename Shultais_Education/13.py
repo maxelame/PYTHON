85d6ff4d0dca29152f509a8c91d605a459d15f42
maxelame <maxelame31@gmail.com>
2022-11-30 11:21:44 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -167,3 +167,17 @@ def convert_to_miles(km):
 num = int(input())
 # вызываем функцию
 print(convert_to_miles(num))
+
+
+# объявление функции
+def get_days(month):
+    if month in [1,3,5,7,8,10,12]:
+        return "31"
+    elif month == 2:
+        return "28"
+    else:
+        return "30"
+# считываем данные
+num = int(input())
+# вызываем функцию
+print(get_days(num))
