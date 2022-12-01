6461001c7578e4f2fa315e1e60b83205833d59a2
maxelame <maxelame31@gmail.com>
2022-12-01 07:33:45 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -200,3 +200,9 @@ n = int(input())
 
 # вызываем функцию
 print(get_factors(n))
+
+
+
+def number_of_factors(num):
+    return len([i for i in range(1, num + 1) if num % i == 0])
+print(number_of_factors(int(input())))
