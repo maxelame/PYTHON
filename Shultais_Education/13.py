820bcdfdb661cc91a2644fe6889b5a03d2d3e6d3
maxelame <maxelame31@gmail.com>
2022-12-02 08:33:46 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -235,3 +235,19 @@ n = int(input())
 
 # вызываем функцию
 print(number_of_factors(n))
+
+
+
+# объявление функции
+def find_all(target, symbol):
+    l = []
+    for i in range(len(target)):
+        if target[i] == symbol:
+            l.append(i)
+    return l
+# считываем данные
+s = input()
+char = input()
+
+# вызываем функцию
+print(find_all(s, char))
