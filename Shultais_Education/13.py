f591e8eaa2df314c5801370d348cf1307a9b801d
maxelame <maxelame31@gmail.com>
2022-12-04 09:02:44 +0300

    new task

--- Shultais_Education/13.py
+++ Shultais_Education/13.py
@@ -272,3 +272,32 @@ for i in range(n):
     s += list(map(int, input().split()))
 s.sort()
 print(*s, sep = " ")
+
+
+
+# put your python code here
+def quick_merge(list1, list2):
+    result = []
+    p1 = 0  # указатель на первый элемент списка list1
+    p2 = 0  # указатель на первый элемент списка list2
+
+    while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+        if list1[p1] <= list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+        else:
+            result.append(list2[p2])
+            p2 += 1
+
+    if p1 < len(list1):   # прицепление остатка
+        result += list1[p1:]
+    if p2 < len(list2):
+        result += list2[p2:]
+    return result
+n = int(input())
+list1 = [int(c) for c in input().split()]
+while n - 1 != 0:
+    list2 = [int(c) for c in input().split()]
+    list1 = quick_merge(list1, list2)
+    n -= 1
+print(*list1)
