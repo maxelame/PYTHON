6869c47bdaa3ed0ba6afde9c7ac3309f5a11b8b6
maxelame <maxelame31@gmail.com>
2022-12-06 07:44:00 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -14,3 +14,15 @@ n = int(input())
 
 # вызываем функцию
 print(is_prime(n))
+
+
+
+# объявление функции
+def is_prime(num):
+    return len([i for i in range(1, num+1) if num % i == 0]) == 2
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(is_prime(n))
