fa037e88dcc702afe4899d129e6f403635e2b262
maxelame <maxelame31@gmail.com>
2022-12-09 07:46:26 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -69,3 +69,27 @@ txt = input()
 
 # вызываем функцию
 print(is_password_good(txt))
+
+
+
+
+
+
+# объявление функции
+def is_one_away(word1, word2):
+    if len(word1) == len(word2):
+        counter = 0
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                counter += 1
+        if counter == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+# считываем данные
+txt1 = input()
+txt2 = input()
+# вызываем функцию
+print(is_one_away(txt1, txt2))
