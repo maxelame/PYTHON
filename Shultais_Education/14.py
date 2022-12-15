b5dbcc4dfe2a6f149efd401373464fb3c10a7f56
maxelame <maxelame31@gmail.com>
2022-12-15 07:28:35 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -293,3 +293,15 @@ r = float(input())
 # вызываем функцию
 length, square = get_circle(r)
 print(length, square)
+
+
+
+def get_circle(radius):
+    from math import pi
+    c = 2 * pi * radius
+    s = pi * radius ** 2
+    return (c, s)
+
+r = float(input())
+length, square = get_circle(r)
+print(length, square)
