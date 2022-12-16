d772b61df29eae8f56cf357a927663ab56655688
maxelame <maxelame31@gmail.com>
2022-12-16 07:41:16 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -305,3 +305,26 @@ def get_circle(radius):
 r = float(input())
 length, square = get_circle(r)
 print(length, square)
+
+
+
+
+
+from math import *
+def solve(a, b, c):
+    D = b ** 2 - 4*a*c
+    if D < 0:
+        return ("Нет корней")
+    elif D == 0:
+        return -b/(2*a), -b/(2*a)
+    else:
+        x1 = (-b-sqrt(D))/(2*a)
+        x2 = (-b+sqrt(D))/(2*a)
+        return min(x1, x2), max(x1,x2)
+
+# считываем данные
+a, b, c = int(input()), int(input()), int(input())
+
+# вызываем функцию
+x1, x2 = solve(a, b, c)
+print(x1, x2)
