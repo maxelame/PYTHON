4cf551dac5a70c56301a583a04b74ae157903238
maxelame <maxelame31@gmail.com>
2022-12-17 09:00:21 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -350,3 +350,16 @@ def draw_triangle():
 
 # основная программа
 draw_triangle()  # вызов функции
+
+
+
+
+# объявление функции
+def get_shipping_cost(quantity):
+    return 1000 + 120 * (quantity -1)
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(get_shipping_cost(n))
