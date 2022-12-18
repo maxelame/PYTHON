ff2d4410305ae1a7986948e225a2eedb2889bab5
maxelame <maxelame31@gmail.com>
2022-12-18 09:33:24 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -453,3 +453,15 @@ print('Бросаем кубики... ')
 print('Значения граней:')
 print(random.randint(1, 6))
 print(random.randint(1, 6))
+
+
+
+import random
+
+again = 'д'
+while again.lower() == 'д':
+    print('Бросаем кубики... ')
+    print('Значения граней:')
+    print(random.randint(1, 6))
+    print(random.randint(1, 6))
+    again = input('Бросить кубики еще раз? (д = да, н = нет): ')
