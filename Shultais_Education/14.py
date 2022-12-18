cde0000f50fbc847ed0bed14a55ae842395e9c20
maxelame <maxelame31@gmail.com>
2022-12-18 09:30:22 +0300

    new task

--- Shultais_Education/14.py
+++ Shultais_Education/14.py
@@ -437,3 +437,12 @@ text = input()
 
 # вызываем функцию
 print(is_pangram(text))
+
+
+
+import random
+
+random.seed(17)   # явно устанавливаем начальное значение для генератора случайных чисел
+
+for _ in range(10):
+    print(random.randint(1, 100))
