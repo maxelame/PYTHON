fe89e349e2d2a3a445e28a591e28c22b334ea3fc
maxelame <maxelame31@gmail.com>
2023-01-08 09:02:39 +0300

    new task

--- Shultais_Education/15.py
+++ Shultais_Education/15.py
@@ -197,3 +197,11 @@ import re
 pattern = "anton"
 result = [i+1 for i in range(int(input())) if re.findall(r'\w*a\w*n\w*t\w*o\w*n\w*', input())]
 print(*result)
+
+
+
+numbers = [-6, -8, 0, 1, 3, 8, -7, 12, 17, 24, 25, 3, 5, 1]
+res = 0
+for num in numbers:
+    res += (num % 2 == 1) and (num > 1)
+print(res)
