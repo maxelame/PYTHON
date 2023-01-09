a1de75ab5a2228edcc413ce6834e2135a822d78f
maxelame <maxelame31@gmail.com>
2023-01-09 07:17:18 +0300

    new task

--- Shultais_Education/15.py
+++ Shultais_Education/15.py
@@ -211,3 +211,17 @@ print(bool('abc'))
 
 
 print(bool(list(range(10))))
+
+
+# объявление функции
+def func(num1, num2):
+    return num1 % num2 == 0
+
+# считываем данные
+num1, num2 = int(input()), int(input())
+
+# вызываем функцию
+if func(num1, num2):
+    print("делится")
+else:
+    print("не делится")
