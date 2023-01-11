42b0f1d2a1b4a8251639e568995319af12cab3c9
maxelame <maxelame31@gmail.com>
2023-01-11 05:52:11 +0300

    new task

--- Shultais_Education/15.py
+++ Shultais_Education/15.py
@@ -240,3 +240,10 @@ if func(num1, num2):
     print("делится")
 else:
     print("не делится")
+
+
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+print(numbers[2:5])
+print(numbers[:4])
+print(numbers[3:])
