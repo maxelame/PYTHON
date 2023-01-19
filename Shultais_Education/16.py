44ae130c3e3a15a31f2cbd54d2b6da1f6132acf4
maxelame <maxelame31@gmail.com>
2023-01-19 07:26:30 +0300

    new task

--- Shultais_Education/16.py
+++ Shultais_Education/16.py
@@ -18,3 +18,6 @@ for i in list1:
   if max(i) > maximum:
     maximum = max(i)
 print(maximum)
+
+
+print(max([max(i) for i in [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]]))
