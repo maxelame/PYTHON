0882a472f33ead3a22ced5cdd9ad7497f5cf3b98
maxelame <maxelame31@gmail.com>
2023-01-20 07:25:57 +0300

    new task

--- Shultais_Education/16.py
+++ Shultais_Education/16.py
@@ -34,3 +34,13 @@ for i in range(5):
     list1[i].reverse()
 
 print(list1)
+
+
+
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+total = 0
+counter = 0
+for i in list1:
+    total += sum(i)
+    counter += len(i)
+print(total / counter)
