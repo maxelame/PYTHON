332fc1c1091335cb36a3ab6b60462428dda9eb4e
maxelame <maxelame31@gmail.com>
2023-01-22 07:46:04 +0300

    new task

--- Shultais_Education/16.py
+++ Shultais_Education/16.py
@@ -91,3 +91,18 @@ for row in my_list:
     minimum = min(row)
 
 print(maximum + minimum) # 107
+
+
+
+my_list = [[12, 221, 3], [41, 5, 633], [71, 8, 99]]
+
+maximum = my_list[0][0]
+minimum = my_list[0][0]
+
+for row in my_list:
+    if max(row) > maximum:
+        maximum = max(row)
+    if min(row) < minimum:
+        minimum = min(row)
+
+print(maximum + minimum) # 636
