274ca129b6317115dbd315ebd40e80118e73019f
maxelame <maxelame31@gmail.com>
2023-01-22 07:44:27 +0300

    new task

--- Shultais_Education/16.py
+++ Shultais_Education/16.py
@@ -78,3 +78,16 @@ for row in my_list:
     total += sum(row)
 
 print(total) # type(row) integer
+
+
+
+my_list = [[12, 221, 3], [41, 5, 633], [71, 8, 99]]
+
+maximum = my_list[0][0]
+minimum = my_list[0][0]
+
+for row in my_list:
+    maximum = max(row)
+    minimum = min(row)
+
+print(maximum + minimum) # 107
