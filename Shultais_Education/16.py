6b2aeddb297af29f365cd31c8fca1e71f9af4618
maxelame <maxelame31@gmail.com>
2023-01-23 07:10:24 +0300

    new task

--- Shultais_Education/16.py
+++ Shultais_Education/16.py
@@ -142,3 +142,7 @@ print(*[list(range(1, i + 1)) for i in range(1, n + 1)], sep = '\n')
 
 
 print(*[[j for j in range(1, i + 1)] for i in range(1, int(input()) + 1)], sep = '\n')
+
+
+
+print('\n'.join(map(str, [[i for i in range(1, j + 1)] for j in range(1, int(input()) + 1)])))
