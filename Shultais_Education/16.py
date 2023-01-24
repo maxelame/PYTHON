658233e5f90983c437ade988ddad490765660d0e
maxelame <maxelame31@gmail.com>
2023-01-24 07:29:07 +0300

    new task

--- Shultais_Education/16.py
+++ Shultais_Education/16.py
@@ -146,3 +146,21 @@ print(*[[j for j in range(1, i + 1)] for i in range(1, int(input()) + 1)], sep =
 
 
 print('\n'.join(map(str, [[i for i in range(1, j + 1)] for j in range(1, int(input()) + 1)])))
+
+
+# -------------------ФУНКЦИЯ-------------------
+def pascal(n):
+    triangle = [[1]]
+
+    for i in range(n):
+        row = [1]
+        for j in range(1, len(triangle[i])):
+            row += [sum(triangle[i][j - 1: j + 1])]
+        row += [1]
+        triangle.append(row.copy())
+
+    return triangle[n]
+
+
+# --------------------ВЫЗОВ--------------------
+print(pascal(int(input())))
