1480f2f814e7eb98a5c838e0f38189cec12c7b6f
maxelame <maxelame31@gmail.com>
2023-01-30 08:58:27 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -8,3 +8,14 @@ matrix  = [[1, 2, 8, 0],
            [41, 71, 2, -2]]
 
 print(matrix[2][3])
+
+
+n = 3
+a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+for i in range(n):
+    for j in range(n):
+        print(a[i][j], end=' ')
+    print()
