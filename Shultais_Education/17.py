0a9540deb481f1e07dd5ac972f3f7b5bd5213970
maxelame <maxelame31@gmail.com>
2023-01-31 07:08:12 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -41,3 +41,21 @@ for i in range(n):
     for j in range(n):
         print(a[n - i - 1][n - j - 1], end=' ')
     print()
+
+
+n = 5
+a = [[19, 21, 33, 78, 99],
+     [41, 53, 66, 98, 76],
+     [79, 80, 90, 60, 20],
+     [33, 11, 45, 67, 90],
+     [45, 67, 12, 98, 23]]
+
+maximum = -1
+minimum = 100
+
+for i in range(n):
+    if a[i][i] > maximum:
+        maximum = a[i][i]
+    if a[i][n - i - 1] < minimum:
+        minimum = a[i][n - i - 1]
+print(minimum + maximum)
