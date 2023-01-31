48986ca6e14fcf9f3d0f18361ebd19980b520039
maxelame <maxelame31@gmail.com>
2023-01-31 06:58:52 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -30,3 +30,14 @@ for i in range(n):
     for j in range(n):
         print(a[j][i], end=' ')
     print()
+
+
+n = 3
+a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+for i in range(n):
+    for j in range(n):
+        print(a[n - i - 1][n - j - 1], end=' ')
+    print()
