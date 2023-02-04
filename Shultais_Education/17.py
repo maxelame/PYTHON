4b16d3fef9d970a92dbddb7f2c4f14fdab212446
maxelame <maxelame31@gmail.com>
2023-02-04 09:53:39 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -122,7 +122,7 @@ for i in range(n):
     print(counter)
 
 
-
+======================================
 # оцифровываем матрицу
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
