1b944ddf1e69061cc4c38140f9048f947d1cfcd4
maxelame <maxelame31@gmail.com>
2023-02-08 09:00:31 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -235,3 +235,14 @@ for i in range(n):
 
 =================================================
 
+n, m = int(input()), int(input())
+mult = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        mult[i][j] = i * j
+
+for i in range(n):
+    for j in range(m):
+        print(str(mult[i][j]).ljust(3), end=' ')
+    print()
