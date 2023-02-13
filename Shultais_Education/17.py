bbd896ece695cf57fff96e19fa52b25864583196
maxelame <maxelame31@gmail.com>
2023-02-13 09:42:27 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -339,3 +339,13 @@ for i in range(n // 2):
         matrix[i][j], matrix[n-i-1][j] = matrix[n-i-1][j], matrix[i][j]
 for _ in matrix:
     print(*_)
+
+=================================================
+
+n = int(input())
+
+matrix = [[int(item) for item in input().split()] for _ in range(n)]
+matrix.reverse()
+
+for row in matrix:
+    print(*row)
