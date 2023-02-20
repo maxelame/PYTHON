1610bcba0c60d51fd8b5549bc50846b527f7829e
maxelame <maxelame31@gmail.com>
2023-02-20 08:54:29 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -560,3 +560,12 @@ for x in res:
     print(*x)
 
 =================================
+
+n = int(input())
+matrix = [[0] * n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if (i <= j and i <= n-1-j) or (i >= j and i >= n-1-j):
+            matrix[i][j] = 1
+for row in matrix:
+    print(*row)
