04e7979a5afef978cdb7f42be070767fcc10a8ca
maxelame <maxelame31@gmail.com>
2023-02-27 08:07:50 +0300

    new task

--- Shultais_Education/17.py
+++ Shultais_Education/17.py
@@ -731,3 +731,17 @@ for row in matrix2:
     print(*row)
 
 ====================================
+
+n = int(input())
+matrix = [["."]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if i==j or i==n-1-j or i==n//2 or j==n//2:
+            matrix[i][j] = "*"
+for row in matrix:
+    print(*row)
+
+====================================
+
+
+
