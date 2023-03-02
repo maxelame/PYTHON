8211d8e50c9efdd2995adf04f51d9ca69599a026
maxelame <maxelame31@gmail.com>
2023-03-02 08:23:37 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -12,3 +12,10 @@ print(numbers)
 countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
 last = countries[-1]
 print(last)
+
+primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
+print(primes[:6])
+
+
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+print(countries[2:])
