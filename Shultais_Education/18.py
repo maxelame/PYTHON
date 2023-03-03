d4a60b70ffe8bc39756b690f75a1fa564e8f20c1
maxelame <maxelame31@gmail.com>
2023-03-03 08:26:51 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -35,3 +35,8 @@ print(countries[3:-2])
 countries = ('Romania', 'Poland', 'Estonia', 'Bulgaria', 'Slovakia', 'Slovenia', 'Hungary')
 number = len(countries)
 print(number)
+
+
+numbers = (12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324)
+
+print(max(numbers) + min(numbers))
