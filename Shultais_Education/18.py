766539d266d1ed93479a18ec97ead9c7245ceb34
maxelame <maxelame31@gmail.com>
2023-03-05 09:05:08 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -61,3 +61,13 @@ non_empty_tuples = [i for i in tuples if len(i) != 0]
 
 print(non_empty_tuples)
 
+
+tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
+new_tuples = [i[:-1] + (100,) for i in tuples]
+print(new_tuples)
+
+
+tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
+new_tuples = [i[:-1] + (100,) for i in tuples]
+print(new_tuples)
+
