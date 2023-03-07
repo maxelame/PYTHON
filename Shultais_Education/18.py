4c44454db8a7aa7ab59d253ed796a97a5aeb8ee0
maxelame <maxelame31@gmail.com>
2023-03-07 06:52:29 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -101,3 +101,11 @@ for i in range(len(poets)):
 
 print(poets[0])
 print(poets[-1])
+
+
+
+numbers = (2, 3, 5, 7, -11, 13, 17, 19, 23, 29, 31, -6, 41, 43, 47, 53, 59, 61, -96, 71, 1000, -1)
+result = 1
+for i in numbers:
+    result *= i
+print(result)
