621360542d07289481aeba8927430337d2e361a2
maxelame <maxelame31@gmail.com>
2023-03-07 07:23:20 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -109,3 +109,11 @@ result = 1
 for i in numbers:
     result *= i
 print(result)
+
+
+from functools import reduce
+numbers = (2, 3, 5, 7, -11, 13, 17, 19, 23, 29, 31, -6, 41, 43, 47, 53, 59, 61, -96, 71, 1000, -1)
+print(reduce(lambda a, b: a * b, numbers))
+
+data = 'Python для продвинутых!'
+print(tuple(data))
