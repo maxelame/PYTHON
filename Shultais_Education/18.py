7cfd1c42443c92ca6aa58fd789c3b9a0b5fcf5ac
maxelame <maxelame31@gmail.com>
2023-03-08 07:39:43 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -117,3 +117,11 @@ print(reduce(lambda a, b: a * b, numbers))
 
 data = 'Python для продвинутых!'
 print(tuple(data))
+
+
+poet_data = ('Пушкин', 1799, 'Санкт-Петербург')
+poet_data_list = list(poet_data)
+poet_data_list[2] = "Москва"
+poet_data = tuple(poet_data_list)
+
+print(poet_data)
