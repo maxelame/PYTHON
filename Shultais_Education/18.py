611b1514aa98e809859ba9b6ba01a5123b6a226b
maxelame <maxelame31@gmail.com>
2023-03-10 08:36:24 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -149,5 +149,9 @@ print((-b/(2*a), (4*a*c - b ** 2)/(4*a)))
 
 a, b, c = 10, 20, 30
 c, b, a = a + b, b*2, a + b + c
-
 print(a, b, c)
+
+
+points = [('матан', 100), ('линал', 98), ('ангем', 90)]
+subject, value = points[1]
+print(subject, value)
