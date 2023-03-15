a4ba191b48f601b91ec00c930cd4f5552091222e
maxelame <maxelame31@gmail.com>
2023-03-15 05:47:13 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -210,3 +210,19 @@ for i in range(n):
 points = [('матан', 100), ('линал', 98), ('ангем', 90)]
 subject, value = points[1]
 print(subject, value)
+
+
+poets = [
+    ('Тургенев', 14),
+    ('Есенин', 13),
+    ('Маяковский', 28),
+    ('Фет', 15),
+    ('Лермонтов', 20)]
+
+for i in range(len(poets)):
+    for j in range(i+1, len(poets)):
+        if poets[i] > poets[j]:
+            poets[i], poets[j] = poets[j], poets[i]
+
+print(poets[0])
+print(poets[-1])
