5b8fe39b86be6efcd697874a606b25e8fb29d542
maxelame <maxelame31@gmail.com>
2023-03-17 07:59:26 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -231,3 +231,9 @@ print(poets[-1])
 points = [('матан', 100), ('линал', 98), ('ангем', 90)]
 subject, value = points[1]
 print(subject, value)
+
+
+
+points = [('матан', 100), ('линал', 98), ('ангем', 90)]
+subject, value = points[1]
+print(subject, value)
