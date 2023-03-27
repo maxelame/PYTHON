577a90c240c42f0a8b3a0775fd81381f4d18404d
maxelame <maxelame31@gmail.com>
2023-03-27 08:52:32 +0300

    new task

--- Shultais_Education/18.py
+++ Shultais_Education/18.py
@@ -311,3 +311,5 @@ print(summ)
 fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
 sorted_fruits = sorted(fruits, reverse = True)
 print(*sorted_fruits, sep = "\n")
+
+print(len(set(input())))
