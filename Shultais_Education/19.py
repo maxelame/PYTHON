60359f7b452933db2b489a00195e1df91be250a9
maxelame <maxelame31@gmail.com>
2023-04-20 06:24:53 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -43,6 +43,6 @@ print(*(sorted(map(int, set12), reverse = True)))
 
 set1, set2, set3 = set(input().split()), set(input().split()), set(input().split())
 set4 = set1 & set2 & set3 # Находим значения которые повторяются сразу у ТРЕХ детёнышей
-set_full = set1 | set2 | set3
+set_full = set1 | set2 | set3 # Находим общий список оценок
 set_full.difference_update(set4)
 print(*(sorted(map(int, set_full))))
