f62dd8c466272e260f70db8ba7db12edbba1d8ac
maxelame <maxelame31@gmail.com>
2023-04-24 22:36:31 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -69,3 +69,5 @@ digits = {int(c) for c in input()}
 items = [10, '30', 30, 10, '56', 34, '12', 90, 89, 34, 45, '67', 12, 10, 90, 23, '45', 56, '56', 1, 5, '6', 5]
 set1 = {int(c) for c in items}
 print(*sorted(set1))
+
+
