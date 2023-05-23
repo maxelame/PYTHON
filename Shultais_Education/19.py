31c120d6129d058a57ef7907e14360f57fb0f504
maxelame <maxelame31@gmail.com>
2023-05-23 09:02:19 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -477,3 +477,14 @@ word = input().upper()
 for c in word:
     if c in mydict:
         print(mydict[c], end=' ')
+
+
+
+numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10, 4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
+
+result = {}
+for num in numbers:
+    if num not in result:
+        result[num] = 1
+    else:
+        result[num] += 1
