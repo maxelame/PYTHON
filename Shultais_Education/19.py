34b35e96707808bef7b38076eff43b3dcca30a2c
maxelame <maxelame31@gmail.com>
2023-05-23 09:25:27 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -488,3 +488,14 @@ for num in numbers:
         result[num] = 1
     else:
         result[num] += 1
+
+
+
+# упрощаем
+
+
+numbers = [9, 8, 32, 1, 10, 1, 10, 23, 1, 4, 10, 4, 2, 2, 2, 2, 1, 10, 1, 2, 2, 32, 23, 23]
+
+result = {}
+for num in numbers:
+    result[num] = result.get(num, 0) + 1
