b7a4acece75481c4a328aef86a8337caa6252874
maxelame <maxelame31@gmail.com>
2023-05-28 23:03:39 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -570,3 +570,10 @@ dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z
 for c in dict2:
     dict1[c] = dict2.get(c, 0) + dict1.get(c, 0)
 result = dict1.copy()
+
+
+
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+result = {k: dict1.get(k, 0) + dict2.get(k, 0) for k in set(dict1.keys()).union(dict2.keys())}
