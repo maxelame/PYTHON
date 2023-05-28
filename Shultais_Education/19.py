94f80e9efa511136d516019eec5f06a29891a051
maxelame <maxelame31@gmail.com>
2023-05-28 23:03:18 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -562,3 +562,11 @@ for key in dict2:
         result[key] += dict2[key]
     else:
         result[key] = dict2[key]
+
+
+
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+for c in dict2:
+    dict1[c] = dict2.get(c, 0) + dict1.get(c, 0)
+result = dict1.copy()
