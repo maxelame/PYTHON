692bb9eac074748a9d86abc1ef11e05bd2853bde
maxelame <maxelame31@gmail.com>
2023-05-28 23:02:45 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -551,3 +551,14 @@ print(student)
 
 
 result = {i: i ** 2 for i in range(1, 16)}
+
+
+
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+result = dict1.copy()
+for key in dict2:
+    if key in result:
+        result[key] += dict2[key]
+    else:
+        result[key] = dict2[key]
