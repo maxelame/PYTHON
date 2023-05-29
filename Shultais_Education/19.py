ab09f382fc0838d616a314ce0742a42c6cf1067b
maxelame <maxelame31@gmail.com>
2023-05-29 08:45:03 +0300

    new task

--- Shultais_Education/19.py
+++ Shultais_Education/19.py
@@ -577,3 +577,8 @@ dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q':
 dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
 
 result = {k: dict1.get(k, 0) + dict2.get(k, 0) for k in set(dict1.keys()).union(dict2.keys())}
+
+
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+
+result = {k: text.count(k) for k in set(text)}
