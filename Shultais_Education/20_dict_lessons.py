92835727cef98a2b0e19a256c1b572361289f108
maxelame <maxelame31@gmail.com>
2023-06-19 08:00:32 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -5,3 +5,10 @@ months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'Jun
 result = {months[i] : i for i in months}
 
 ####
+#В переменной s хранится строка пар число:слово. Дополните приведенный код, используя
+# генератор, чтобы получить словарь result , в котором числа будут ключами, а слова – значениями.
+s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+result = {int(i.split(":")[0]) : i.split(":")[1] for i in s.split()}
+
+
+####
