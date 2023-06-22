a1b91f9abf4c9de357cc93aa0a439ecd536bb506
maxelame <maxelame31@gmail.com>
2023-06-22 08:02:21 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -44,3 +44,8 @@ result = {i[0]: (i[1],i[2]) for i in tuples}
 
 
 #####
+student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013']
+student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
+student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+
+result = [{i: {j: k}} for i, j, k in zip(student_ids, student_names, student_grades)] # для соединения используем функцию ZIP
