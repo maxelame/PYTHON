7860694c61b79c0c4b447c2d5fc6aa65157e3e72
maxelame <maxelame31@gmail.com>
2023-06-30 08:19:43 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -96,3 +96,9 @@ for _ in range(n):
     print("Орел" if random.randint(0, 1) else "Решка")
 
 #####
+import random
+n = int(input())    # количество попыток
+for _ in range(n):
+    print(random.randint(1, 6))
+
+#####
