fac01cda68826f8f5640fdd384a58438f82f2ce3
maxelame <maxelame31@gmail.com>
2023-07-01 16:44:05 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -106,3 +106,5 @@ import random
 length = int(input())    # длина пароля
 for _ in range(length):
     print((chr(random.choice([random.randint(65,90), random.randint(97,122)]))), end = '')
+
+#####
