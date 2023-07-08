bd48775dc223a405c3c3fe72990a03a9687733a6
maxelame <maxelame31@gmail.com>
2023-07-08 09:27:28 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -142,3 +142,13 @@ def generate_ip():
 print(generate_ip())
 
 #####
+import random
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+random.shuffle(matrix)
+print(matrix)
+
+
+
