eb28264739ed08e41a2d54f5b8991f6b26eff99e
maxelame <maxelame31@gmail.com>
2023-07-15 10:20:05 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -283,4 +283,11 @@ for i in dnk:
 print(rnk)
 
 ######
-
+lst = input().split()
+dict1 = {}
+for i in lst:
+    if i in dict1:
+        dict1[i] += 1
+    else:
+        dict1[i] = 1
+    print(dict1[i], end=" ")
