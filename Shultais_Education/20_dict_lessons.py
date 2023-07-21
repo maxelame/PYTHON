52755aba96265e994abe357903f5dc3b11ca9850
maxelame <maxelame31@gmail.com>
2023-07-21 08:07:31 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -439,3 +439,19 @@ for _ in range(n):
 print((k/n)*s0)
 
 ######
+
+import random
+
+n = 10**6       # количество испытаний
+k = 0
+s0 = 4
+for _ in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+
+    if x**2 + y**2 <= 1:
+        k += 1
+
+print((k/n)*s0)
+
+#####
