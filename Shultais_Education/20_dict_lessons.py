fa1b39728c4221f13e3c3dc8bbb5e4403d907cb0
maxelame <maxelame31@gmail.com>
2023-07-29 08:42:07 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -548,3 +548,14 @@ if num == 0:
     print('YES')
 else:
     print('NO')
+
+#####
+
+from decimal import *
+
+num = Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3')
+
+if num == 0:
+    print('YES')
+else:
+    print('NO')
