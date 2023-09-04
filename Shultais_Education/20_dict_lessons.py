6b04047fd4ad15867b647ab376d9067c360e38dc
maxelame <maxelame31@gmail.com>
2023-09-04 08:43:51 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -681,3 +681,11 @@ from fractions import Fraction as F
 print(sum([F(1, factorial(i)) for i in range(1, int(input()) + 1)]))
 
 ######
+
+from math import gcd
+from fractions import Fraction as F
+n, lst = int(input()), []
+for i in range(1, n + 1):
+    if gcd(i, n-i) == 1 and i < n-i:
+        lst += [F(i, n-i)]
+print(lst[-1])
