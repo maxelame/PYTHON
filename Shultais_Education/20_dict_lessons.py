0fadb952d35d9fe6257b8ae11bf269feb6e8d9d6
maxelame <maxelame31@gmail.com>
2023-09-07 08:56:50 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -710,3 +710,13 @@ z4 = complex(5, 0)        # z4 = 5 + 0j
 z5 = complex('3+4j')      # создание на основе строки
 
 print(z1, z2, z3, z4, z5, sep='\n')
+
+
+######
+z1 = 5 + 7j
+z2 = 1j
+z3 = -3 + 5J
+z4 = 1.5 + 3.2j
+
+print(z1, z2, z3, z4, sep='\n')
+print(type(z1))
