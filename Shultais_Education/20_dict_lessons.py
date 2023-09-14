aca2c4fc1fbebfe99a7c4fe47a67c6be9bd1fffa
maxelame <maxelame31@gmail.com>
2023-09-14 07:55:35 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -760,3 +760,9 @@ z1, z2 = input(), input()
 print(f"{complex(z1)} + {complex(z2)} = {complex(z1) + complex(z2)}")
 print(f"{complex(z1)} - {complex(z2)} = {complex(z1) - complex(z2)}")
 print(f"{complex(z1)} * {complex(z2)} = {complex(z1) * complex(z2)}")
+
+#####
+numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+dict1 = {abs(i) : i for i in numbers}
+print(dict1[max(dict1)])
+print(max(dict1))
