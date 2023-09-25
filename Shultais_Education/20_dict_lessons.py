f0542748b5ead0da9ff9fd98eea72bce2be3c492
maxelame <maxelame31@gmail.com>
2023-09-25 08:05:09 +0300

    new task

--- Shultais_Education/20_dict_lessons.py
+++ Shultais_Education/20_dict_lessons.py
@@ -821,5 +821,12 @@ def fancy(length, char1='-', char2='*'):
 
 print(fancy(3))
 
+######
+
+def fancy(length, char1='-', char2='*'):
+    return (char1 + char2) * length + char1
+
+print(fancy(char2='$', length=3))
+
 
 
