4bc1de1c5ab8c75330a0dbcc8dfb5410db3c09ed
maxelame <maxelame31@gmail.com>
2023-09-26 09:27:30 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -6,3 +6,12 @@ def fancy(length, char1='-', char2='*'):
 print(fancy(char2='!'))
 
 ######
+
+def matrix(n =1,m = 0, a = 0):
+    if n == 1 and not m:
+        m = 1
+    elif n != 1 and not m:
+        m = n
+    return [[a]*m for _ in range(n)]
+
+######
