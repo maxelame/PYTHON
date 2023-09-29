7357dfdad4dfe8461f0f86e815e2a7f994a36f24
maxelame <maxelame31@gmail.com>
2023-09-29 08:32:57 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -35,3 +35,11 @@ print(func(10, 20, 30, 40, 50, 60))
 
 
 #####
+
+def func(*args):
+    return max(args) + min(args)
+
+
+print(func(10, 15, *[31, 42, 5, 1], *(17, 28, 19, 100), 13, 12))
+
+######
