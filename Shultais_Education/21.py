fa4f9d52dc8ecbbb731f2bfee7ad82a6d33f83e2
maxelame <maxelame31@gmail.com>
2023-10-14 22:26:38 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -144,3 +144,10 @@ s3 = 'beegeek'
 
 print(min(s1, s2, s3))
 print(max(s1, s2, s3))
+
+s1 = 'python'
+s2 = 'stepicon'
+s3 = 'beegeek'
+
+print(min(s1, s2, s3, key=len))
+print(max(s1, s2, s3, key=len))
