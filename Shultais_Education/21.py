f0bffdec363a79dd2ee2cf2b495c7b8d66eb4ebd
maxelame <maxelame31@gmail.com>
2023-10-16 20:58:06 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -176,3 +176,12 @@ def comparator(pair):
 pairs = [(5, 4), (3, 2), (1, 7), (8, 2)]
 pairs.sort(key=comparator)
 print(pairs)
+
+
+
+
+def comparator(pair):
+    return pair[0] + pair[1]
+pairs = [(5, 4), (3, 2), (1, 7), (8, 2)]
+pairs.sort(key=comparator, reverse=True)
+print(pairs)
