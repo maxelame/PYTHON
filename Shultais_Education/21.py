7be3951a38cab879bf57bdb7276cd96094064a92
maxelame <maxelame31@gmail.com>
2023-10-17 09:11:06 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -191,3 +191,17 @@ print(pairs)
 words = ['this', 'is', 'a', 'test', 'of', 'sorting']
 words.sort(key=len)
 print(words)
+
+
+
+def f1(x):
+    return 2*x+1
+def f2(x):
+    return x**2
+def f3(x):
+    return -x**2+1
+def f4(x):
+    return x-3
+funcs = [f1, f2, f3, f4]
+i = int(input())
+print(funcs[i](2))
