61c62a3454d85df8ab906e7c8543132792506ca6
maxelame <maxelame31@gmail.com>
2023-10-21 22:22:52 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -217,3 +217,11 @@ print(numbers[-1])
 
 
 
+points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
+
+def direct(item):
+    return (item[0]**2 + item[-1]**2) ** 0.5
+
+points.sort(key=direct)
+
+print(points)
