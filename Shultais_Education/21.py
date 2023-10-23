686b2c8c3a49f36800b1f7e1caf0675ca9861111
maxelame <maxelame31@gmail.com>
2023-10-23 08:09:15 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -234,3 +234,12 @@ def func(item):
 numbers.sort(key = func)
 
 print(numbers)
+
+
+
+athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30), ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
+
+n = int(input())
+athletes.sort(key = lambda x: x[n-1])
+for i in athletes:
+    print(*i)
