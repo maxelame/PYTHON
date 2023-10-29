a7b466c257bc7a6117ff708938ed71a3817437b0
maxelame <maxelame31@gmail.com>
2023-10-29 13:59:52 +0300

    new task

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -281,7 +281,7 @@ words = ['abba', 'qwerty', 'python', 'a', 'deed', 'nun', 'level', 'deified', 'bb
 words_len = map(len, words)
 print(max(words_len))
 
-Функция map():
+#Функция map():
 
 def map(function, items):
     result = []
@@ -291,3 +291,22 @@ def map(function, items):
 
 
 ####s
+
+def predicate(word):
+    return word == word[::-1]
+
+
+words = ['abba', 'qwerty', 'python', 'a', 'deed', 'nun', 'level', 'language', 'deified', 'bbbbb', 'mother', 'sister', 'surface', '1234321']
+filtered = filter(predicate, words)
+print(len(filtered))
+
+####Функция filter():
+
+def filter(function, items):
+    result = []
+    for item in items:
+        if function(item):
+            result.append(item)
+    return result
+
+######
