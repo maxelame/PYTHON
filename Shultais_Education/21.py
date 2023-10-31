9aee18345edd3e26b5e441a9e62a0bcccbc98575
maxelame <maxelame31@gmail.com>
2023-10-31 22:25:05 +0300

    new

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -327,3 +327,17 @@ def map(function, items):
     return result
 
 ####
+def map(function, items):
+    result = []
+    for item in items:
+        result.append(function(item))
+    return result
+
+
+numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]
+
+numbers = map(lambda x: round(x, 2), numbers)
+print(*numbers, sep = "\n")
+
+
+#####
