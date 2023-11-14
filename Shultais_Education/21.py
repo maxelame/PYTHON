12ed1ae21a613852bdc16b25d414ea000cad85a4
maxelame <maxelame31@gmail.com>
2023-11-14 21:58:29 +0300

    new

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -451,3 +451,12 @@ letters = ['a', 'b', 'd', 'e', 'i', 'j', 'o']
 filtered_vowels = filter(filter_vowels, letters)
 
 print(*filtered_vowels)
+
+
+######
+random_list = [1, 'a', 0, False, True, '0', 7, '']
+filtered_list = list(filter(None, random_list))
+print(filtered_list)
+
+
+#####
