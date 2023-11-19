6e6f0bf0e9d33157cef5490580547db35b98fd1c
maxelame <maxelame31@gmail.com>
2023-11-19 09:54:13 +0300

    new

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -490,3 +490,12 @@ from operator import mul
 
 result = list(map(mul, ['a', 'b', 'c'], [1, 2, 3]))
 print(result)
+
+
+#####
+
+from operator import add
+from functools import reduce
+
+result = reduce(add, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(result)
