b9556fa78b4dca33d4e9f53139acac78e7dc583a
maxelame <maxelame31@gmail.com>
2023-11-20 23:34:30 +0300

    new

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -499,3 +499,12 @@ from functools import reduce
 
 result = reduce(add, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(result)
+
+####
+
+from operator import add
+from functools import reduce
+
+result = reduce(add, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(result)
+
