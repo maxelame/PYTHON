3bc46146b83218594896f69999e52eb3f896b3d8
maxelame <maxelame31@gmail.com>
2023-12-08 21:55:18 +0300

    new

--- Shultais_Education/21.py
+++ Shultais_Education/21.py
@@ -533,3 +533,15 @@ func = lambda x: x[0] in ["a","A"] and x[-1] in ["a","A"]
 
 
 #####
+
+is_non_negative_num = lambda x: x[0] != "-" and x.count('.') < 2 and x.upper() == x.lower()
+
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))
+
