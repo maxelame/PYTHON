8b0180470d6ca261227a9689a45e803c389546f5
maxelame <maxelame31@gmail.com>
2023-12-09 07:37:47 +0300

    new

--- Shultais_Education/22.py
+++ Shultais_Education/22.py
@@ -8,3 +8,17 @@ print(is_non_negative_num('987'))
 print(is_non_negative_num('abcd'))
 print(is_non_negative_num('123.122.12'))
 print(is_non_negative_num('123.122'))
+
+
+#####
+is_num = lambda x: x.count('-') < 2 and x.count('.') < 2 and x.upper() == x.lower() and x.rfind('-') <= 0
+
+print(is_num('10.34ab'))
+print(is_num('10.45'))
+print(is_num('-18'))
+print(is_num('-34.67'))
+print(is_num('987'))
+print(is_num('abcd'))
+print(is_num('123.122.12'))
+print(is_num('-123.122'))
+print(is_num('--13.2'))
