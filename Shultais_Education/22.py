2d0e03be58825518ff5eefef576f26324ee290de
maxelame <maxelame31@gmail.com>
2023-12-12 07:19:12 +0300

    new

--- Shultais_Education/22.py
+++ Shultais_Education/22.py
@@ -40,3 +40,11 @@ num_1 = list(filter(lambda x: x % 2 == 0 or x <= 47, numbers))
 num_2 = list(map(lambda x: x//2 if x % 2 == 0 else x, num_1))
 
 print(*num_2)
+
+
+#####
+
+data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'), (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'), (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'), (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
+
+for pop, city in sorted(data, key=lambda x: x[1][-1], reverse=True):
+    print(f'{city}: {pop}')
