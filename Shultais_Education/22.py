369a851b82ccf40ace3d0df7ffb135b905a108b7
maxelame <maxelame31@gmail.com>
2023-12-22 08:06:53 +0300

    new

--- Shultais_Education/22.py
+++ Shultais_Education/22.py
@@ -137,3 +137,13 @@ for index, number in enumerate(numbers, 1):
 print(total)
 
 ######
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [5, 4, 3, 2, 1]
+
+result = 0
+for x, y in zip(list1, list2):
+    result += x*y
+print(result)
+
+#####
