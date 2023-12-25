29d48cbb39ed14e5859a6a6746b267f95533494f
maxelame <maxelame31@gmail.com>
2023-12-25 17:44:10 +0300

    new

--- Shultais_Education/22.py
+++ Shultais_Education/22.py
@@ -165,3 +165,11 @@ def ignore_command(command):
 def ignore_command(command):
     ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
     return any(i in command for i in ignore)
+
+######
+
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+for country, capital, population in zip(countries, capitals, population):
+    print(f"{capital} is the capital of {country}, population equal {population} people.")
